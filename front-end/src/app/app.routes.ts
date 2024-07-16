5a6ba61625d60c98aa91e4532e3ae824da011054
bllurryyk <khgds1@hotmail.com>
2024-07-16 16:09:11 -0300

    feat: Sobre

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 
 /* pages */
+import { SobreComponent } from './pages/sobre/sobre.component';
 import { ProjetosComponent } from './pages/projetos/projetos.component';
 
 export const routes: Routes = [
-  {path: 'projetos', component: ProjetosComponent}
+  { path: '', component: SobreComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'projetos', component: ProjetosComponent }
 ];
