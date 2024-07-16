2daa70ccb4b710a75bc4e890d59cd3aea5f77b52
bllurryyk <khgds1@hotmail.com>
2024-07-16 16:09:35 -0300

    feat: Component card v1

---
+++ front-end/src/app/components/card-v1/card-v1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-v1',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-v1.component.html',
+  styleUrl: './card-v1.component.css'
+})
+export class CardV1Component {
+
+}
